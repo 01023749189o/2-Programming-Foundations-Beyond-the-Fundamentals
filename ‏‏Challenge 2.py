773fba588025b42241b974f50a324d96734ec2f7
01023749189o <97387353+01023749189o@users.noreply.github.com>
2022-04-19 18:51:01 +0200

    Add files via upload

---
+++ ‏‏Challenge 2.py
@@ -0,0 +1,22 @@
+fruits = [
+    'apples',
+    'bananas',
+    'dragon fruit',
+    'mangos',
+    'nectarines',
+    'pears',
+]
+
+print('Our Fruit Selection:')
+for fruit in fruits:
+    print(fruit)
+
+# Outpot
+# 
+# Our Fruit Selection:
+# apples
+# bananas
+# dragon fruit
+# mangos
+# nectarines
+# pears
---
+++ ‏‏‏‏Challenge 3.py
@@ -0,0 +1,12 @@
+miles = input('Enter a distance in miles: ')
+# kilometers_value = miles_value * 1.609344
+
+milesFloat = float(miles)
+kilometers = milesFloat * 1.609344
+print('That value in kilometers is')
+print(kilometers)
+
+# Outpot
+# 
+# That value in kilometers is50
+# miles * 1.609344
